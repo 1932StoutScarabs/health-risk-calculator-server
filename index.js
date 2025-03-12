efbdf24dc8d59dcb63d1b923a162bbb93009ff7c
rplewi <rlewi0506@gmail.com>
2025-03-12 11:51:43 -0500

    Updated calculations to be more client oriented

--- index.js
+++ index.js
@@ -11,36 +11,14 @@ app.use(express.static(__dirname + '/static'))
 app.use(cors({ origin: '*' }))
 
 // The app.get functions below are being processed in Node.js running on the server.
-
 app.get('/api/ping', (request, response) => {
 	console.log('Calling "/api/ping"')
 	response.type('text/plain')
 	response.send('ping response')
 })
 
-
-// Calculating Risk Function
-/*
-app.get('/ca', (request, response) => {
-	console.log('Calling "/calculate-bmi" on the Node.js server.')
-	var inputs = url.parse(request.url, true).query
-	const heightFeet = parseInt(inputs.feet)
-	const heightInches = parseInt(inputs.inches)
-	const weight = parseInt(inputs.lbs)
-
-	console.log('Height:' + heightFeet + '\'' + heightInches + '\"')
-	console.log('Weight:' + weight + ' lbs.')
-
-	// Todo: Implement unit conversions and BMI calculations.
-	// Todo: Return BMI instead of Todo message.
-
-	response.type('text/plain')
-	response.send('Todo: Implement "/calculate-bmi"')
-})
-*/
-
 app.get('/calc-risk', (req, res) => {
-	res.send(`This is a message hehe! 
+	/*res.send(`This is a message hehe! 
 		---------------------
 		Fam Disease: ${calculateFamDis(2)}
 		---------------------
@@ -51,49 +29,60 @@ app.get('/calc-risk', (req, res) => {
 		BMI Risk: ${calculateBMIRisk(calculateBMI())}
 		---------------------
 		`)
+		*/
+	res.send(calculateRisk(/* This is where we will get the paramters from the user so we can do calculations, IDK how yet though*/))
 
-	
-	
 })
 
-function calculateRisk(){
+function calculateRisk(famDisVal, ageRiskVal, bpVal, BMIHeight, BMIWeight ){
 	// this will be the eventual risk calculation that will be returned through /calc-risk
-	
-}
-
-function calculateFamDis(param1 /* This param is the amount of family disease they have*/){
-	// This will calculate the points associated with family disease.
-	// This value should be the amount of family diseases that a person has, instead of individual, making calculations easier.
-	if (param1 == 1){
-		return parseInt(10)
-	} else if (param1 == 2){
-		return parseInt(20) 
-	} else if (param1 == 3){
-		return parseInt(30)
-	} else {
-		return null //default value for invalid number.
+	try {
+		return({
+			BMI: parseInt(calculateBMI(BMIHeight, BMIWeight)),
+			Risk: calculateFamDis(famDisVal) + calculateAgeRisk(ageRiskVal) + calculateBP(bpVal) + calculateBMIRisk(calculateBMI(BMIHeight, BMIWeight))
+		})
+		// BMI and Risk are the values in the JSON object that we will be returning, to be used in the HTML.
+	} catch (exception){
+		console.log("Something went wrong with the calculation")
+		// Values could have been null :)
 	}
 }
-function calculateAgeRisk(param1 /*This param is the age they are */){
+
+function calculateFamDis(famDisValue /*This is the amount of family disease they have (1-3)*/){
+	// This will calculate the points associated with family disease.
+	// This value should be the amount of family diseases that a person has, instead of individual, making calculations easier.
+	if (famDisValue == 1){
+		return parseInt(10)
+	} else if (famDisValue == 2){
+		return parseInt(20) 
+	} else if (famDisValue == 3){
+		return parseInt(30)
+	} else if (famDisValue == 0) {
+		return parseInt(0)
+	} else {
+		return null //default value for invalid parameter just in case.
+	}
+}
+function calculateAgeRisk(AgeValue /*This param is the age value*/){
 	// This will calculate the points assocaited with age
-	switch (param1){
-		case (param1 < 30):
+	switch (AgeValue){
+		case (AgeValue < 30):
 			//statment here
 			return parseInt(0)
-		case (param1 < 45):
+		case (AgeValue < 45):
 			//statement here
 			return parseInt(10)
-		case (param1 < 60):
+		case (AgeValue < 60):
 			//statement here
 			return parseInt(20)
-		case (param1):
+		case (AgeValue):
 			//statment for value above or below these.	
 			return parseInt(30)
 	}
 }
-function calculateBP(param1 /*This param is the type of bloodpressure */){
+function calculateBP(BPValue /*This param is the type of bloodpressure will be a string */){
 	// This will calculate the points associated with bloodpressure
-	switch (param1){
+	switch (BPValue){
 		case "normal":
 			//statement here all are value points being returned
 			return parseInt(0)
@@ -114,23 +103,24 @@ function calculateBP(param1 /*This param is the type of bloodpressure */){
 
 	}
 }
-function calculateBMIRisk(param1 /* This will be the BMI value */){
+
+function calculateBMIRisk(BMIVal /* This will be the BMI value */){
 	// Switch statements dont support ranges between values so I had to use an if-else statement here.
-	if(param1 >= 18.5 && param1 <= 24.9){
+	if(BMIVal >= 18.5 && BMIVal <= 24.9){
 		return 0
-	}else if(param1 >= 25 && param1 <= 29.9){
+	}else if(BMIVal >= 25 && BMIVal <= 29.9){
 		return 30
-	}else if(param1 >= 30 && param1 <= 34.9){
+	}else if(BMIVal >= 30 && BMIVal <= 34.9){
 		return  75
 	}else{
 		return "err"
 	}
-
 }
-function calculateBMI(param1, param2 /*These will be the input parameters in the future*/){
+
+function calculateBMI(heightIN, weightLBS /*These will be the input parameters in the future*/){
 	console.log("Calculating BMI now!")
-	const weightLBS = 167 // Change these to parameterized values in the future
-	const heightIN = 67 // Change these to parameterized values in the future
+	weightLBS = 167 // Change these to parameterized values in the future
+	heightIN = 67 // Change these to parameterized values in the future
 	bmi = convertToKg(weightLBS) / (convertToM2(heightIN))
 	console.log(bmi)
 	return bmi
@@ -144,8 +134,6 @@ function convertToM2(heightIN){
 	const heightM2 = heightIN2 * 0.00064516
 	return heightM2
 }
-
-
 
 
 // Custom 404 page.
